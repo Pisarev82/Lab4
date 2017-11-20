d51d8bafc605e10ade6ce1364e2a31e791bf3684
dkirsanova <darena13@mail.ru>
2017-11-20 12:33:54 +0300

    Pretty big pictures.

--- app/src/main/java/com/kulbiy/alexey/lab4/PictureFragment.java
+++ app/src/main/java/com/kulbiy/alexey/lab4/PictureFragment.java
@@ -3,13 +3,11 @@ package com.kulbiy.alexey.lab4;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class PictureFragment extends Fragment {
